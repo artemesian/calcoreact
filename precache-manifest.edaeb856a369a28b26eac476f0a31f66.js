398c9e3aad539170cd96221676d301c3e3067695
angeloDiepe <angelodiepe10@yahoo.com>
2019-10-31 04:47:31 +0100

    Updates

---
+++ precache-manifest.edaeb856a369a28b26eac476f0a31f66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba2e346d010ab9199cade170b22f8d7",
+    "url": "/calcoreact/index.html"
+  },
+  {
+    "revision": "596ca5a1a8a2b1fa4c1b",
+    "url": "/calcoreact/static/css/main.f398aad8.chunk.css"
+  },
+  {
+    "revision": "159815ff9b0d39a4d5b4",
+    "url": "/calcoreact/static/js/2.dd124143.chunk.js"
+  },
+  {
+    "revision": "596ca5a1a8a2b1fa4c1b",
+    "url": "/calcoreact/static/js/main.999f9ef3.chunk.js"
+  },
+  {
+    "revision": "bf1482c685ff169de4f4",
+    "url": "/calcoreact/static/js/runtime-main.279ca3b3.js"
+  }
+]);
